549dc911720c46ae3c721c2acce5d5606ec280c4
Dan-Moody <daniel.moody2@gmail.com>
2021-03-10 15:05:49 -0500

    Reverse a string in c

--- Text/ReverseAString/C/ReverseAString.c
+++ Text/ReverseAString/C/ReverseAString.c
@@ -1,6 +1,15 @@
 #include <stdio.h>
-int main() {
-   // printf() displays the string inside quotation
-   printf("Hello, World!");
+#include <string.h>
+int main()
+{
+   char s[100];
+
+   printf("Enter a string to reverse\n");
+   gets(s);
+
+   strrev(s);
+
+   printf("Reverse of the string: %s\n", s);
+
    return 0;
 }
